78664ea2d018e4099e28426af5709107b649f89e
Tiesco2 <dtiekoura@yahoo.fr>
2020-09-04 22:56:45 +0200

    create dashboard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Html from './Components/Html';
+import Css from './Components/Css';
+import Js from './Components/Js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Switch>
+
+          <Route exact path="/" component={Home} />
+          <Route exact path="/html" component={Html} />
+          <Route path="/css" component={Css} />
+          <Route path="/js" component={Js} />
+
+        </Switch>
+        <style />
+        <Header />
+        
       </header>
     </div>
   );
